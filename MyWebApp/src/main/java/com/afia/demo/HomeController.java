18a90d3910aa0b6b099400f49cf5392bb23077a5
afiapython <afia.python@gmail.com>
2020-04-25 12:07:01 -0700

    first commit .

---
+++ MyWebApp/src/main/java/com/afia/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.afia.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+	@ResponseBody
+	public String home() {
+		System.out.println("Good");
+		return "home.jsp";
+	}
+
+}
